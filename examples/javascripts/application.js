3c54873e2e5851d919af6a888b58d40b4b9b22dc
Caleb Brown <cbrown@blenderbox.com>
2012-01-03 10:21:44 -0500

    updated the demo

--- examples/javascripts/application.js
+++ examples/javascripts/application.js
@@ -1,15 +1,26 @@
 var APP = (function($) {
     var app = {};
     app.init = function() {
-        $.htmlhistory.init();
+        // initialize the plugin
+        $.htmlhistory.init({
+            useHistory: true,
+            useHashchange: true,
+            poll: 250,
+            interceptLinks: true,
+            disableHashLinks: true
+        });
+        // bind the 'htmlhistory' event to the window
         $(window).bind('htmlhistory', respondToUrl);
     };
-    function respondToUrl() {
-        console.log('respond to url');
+    function extract(s) {
+        return s.split("#").join("/").split("/").pop();
     }
+    function respondToUrl() {
+        var section = extract(window.location.hash) || extract(window.location.pathname),
+        path = window.location.href.split("#").join("/");
+        // respond to the url however you would like
+        console.log('respond to url', section, path);
+    }
+    $(app.init);
     return app;
 } (jQuery));
-
-$(function() {
-    APP.init();
-});
