0f0a4456955d459a5bd9717a0e9a118b2a337337
Caleb Brown <killerdillercaleb@gmail.com>
2013-05-17 17:15:55 -0300

    Update for formatting and jQuery deprecations

--- src/jquery-html-history.js
+++ src/jquery-html-history.js
@@ -23,20 +23,27 @@
     var evt = 'htmlhistory',
         hash = 'onhashchange',
         hashevt = 'hashchange';
-    
+
     $.fn.htmlhistory = function(handler) {
         return handler ? this.bind(evt, handler) : this.trigger(evt);
     };
+
     var his = $.htmlhistory = {
 
         // default options
         options: {
-            useHistory: true, // whether we use HTML5 History Management to change the current path
-            useHashchange: true, // whether we use HTML5 Hashchange to listen to the URL hash
-            poll: 250, // when using Hashchange in browsers without it, how often to poll the hash (in ms)
-            interceptLinks: true, // do we intercept all relative links to avoid some page reloads?
-            disableHashLinks: true, // do we ensure all links with href=# are not followed (this would mess with our history)?
-            triggerOnLoad: true, // send the hash event on load
+            // whether we use HTML5 History Management to change the current path
+            useHistory: true,
+            // whether we use HTML5 Hashchange to listen to the URL hash
+            useHashchange: true,
+            // when using Hashchange in browsers without it, how often to poll the hash (in ms)
+            poll: 250,
+            // do we intercept all relative links to avoid some page reloads?
+            interceptLinks: true,
+            // do we ensure all links with href=# are not followed (this would mess with our history)?
+            disableHashLinks: true,
+            // send the hash event on load
+            triggerOnLoad: true,
             hash:'#!'
         },
 
@@ -47,14 +54,14 @@
                 $bod = $('body');
 
             $.extend(his.options, options);
-            
+
             // Listen to the HTML5 "popstate" event, if supported and desired
             if (his.options.useHistory && Modernizr.history) {
                 $win.bind('popstate', function(e) {
                     $win.trigger(evt);
                 });
             }
-            
+
             // Listen to the HTML5 "hashevent" event, if supported and desired
             if (his.options.useHashchange && !Modernizr.history) {
                 $win.bind(hashevt, function(e) {
@@ -72,18 +79,18 @@
                 }
                 if (his.options.triggerOnLoad) { $win.trigger(evt); }
             }
-            
+
             // Intercept all relative links on the page, to avoid unneccesary page refreshes
             if (his.options.interceptLinks) {
-                $bod.delegate('a[href=^"/"]', 'click', function(e) {
+                $bod.on('a[href=^"/"]', 'click', function(e) {
                     his.changeTo($(this).attr('href'));
                     e.preventDefault();
                 });
             }
-            
+
             // Ensure all the href=# links on the page don't mess with things
             if (his.options.disableHashLinks) {
-                $bod.delegate('a[href=#]', 'click', function(e) {
+                $bod.on('a[href=#]', 'click', function(e) {
                     e.preventDefault();
                 });
             }
